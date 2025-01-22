ccb2f683973acbfbaba1368ca5791555b84c13ee
Milan Kwiatkowski <milan.mikolaj.kwiatkowski@gmail.com>
2025-01-22 22:41:01 +0100

    setup for tomorrows grind: completed

--- projekt/next.config.ts
+++ projekt/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone", // Optymalizacja dla Vercel
+  reactStrictMode: true,
 };
 
 export default nextConfig;
