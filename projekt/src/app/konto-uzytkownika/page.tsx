94cb75f0ba45ba491edd758b55734f70bd9139c3
Milan Kwiatkowski <milan.mikolaj.kwiatkowski@gmail.com>
2025-01-25 20:04:55 +0100

    minor aesthetic change

--- projekt/src/app/konto-uzytkownika/page.tsx
+++ projekt/src/app/konto-uzytkownika/page.tsx
@@ -3,7 +3,10 @@ import { useEffect, useState } from "react";
 
 export default function KontoUzytkownika() {
   const [storageData, setStorageData] = useState<[string, string][]>([]);
-
+  const profilowe = fota()
+  function fota(){
+    return localStorage.getItem("Profilowe");
+  }
   useEffect(() => {
     const data: [string, string][] = Object.entries(localStorage);
     const data2: [string,string][] = data.filter(
@@ -14,6 +17,11 @@ export default function KontoUzytkownika() {
 
   return (
     <main>
+      <div>{profilowe ? (
+        <img src={profilowe} alt="Obrazek z localStorage" />
+      ) : (
+        <p></p>
+      )}</div>
       <h2>Twoje Konto:</h2>
       <ul>
         {storageData.map(([key, value]) => (
